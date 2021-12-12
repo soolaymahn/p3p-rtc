480a79bfc12c366616bdd95d1d8104b277f1185e
Mohamed Basrai <mohamedalibasrai@gmail.com>
2021-12-12 11:07:54 -0800

    encryption functions

--- src/utils/encryption.js
+++ src/utils/encryption.js
@@ -1,3 +1,63 @@
 export async function initEncryption() {
   console.log({ sjcl: window.sjcl });
 }
+
+/* Encrypt a message */
+export function doEncrypt(password, plaintext, salt) {
+  if (plaintext === "") {
+    return;
+  }
+  if (password.length == 0) {
+    return;
+  }
+
+  var p = {
+    adata: "",
+    iter: 1000,
+    mode: "CCM",
+    ts: 64,
+    ks: 128,
+    iv: [],
+    salt,
+  };
+  return window.sjcl.encrypt(password, plaintext, p, {});
+}
+
+/* Decrypt a message */
+export function doDecrypt(ciphertext, salt, password) {
+  var key;
+
+  if (ciphertext.length === 0) {
+    return;
+  }
+
+  ciphertext = window.sjcl.codec.base64.toBits(ciphertext);
+  if (key.length === 0) {
+    if (password.length) {
+      key = doPbkdf2(128, salt, password);
+    }
+  }
+  var aes = new window.sjcl.cipher.aes(key);
+
+  try {
+    return window.sjcl.codec.utf8String.fromBits(
+      window.sjcl.mode["CCM"].decrypt(aes, ciphertext, [], "", "64")
+    );
+  } catch (e) {
+    return;
+  }
+}
+
+function doPbkdf2(keysize, salt, password) {
+  var p = {
+    iter: 1000,
+    salt,
+  };
+
+  if (password.length === 0) {
+    return;
+  }
+
+  p = window.sjcl.misc.cachedPbkdf2(password, p);
+  return p.key.slice(0, keysize / 32);
+}
