49e36bff7fa9a652958a12cb3045f6e3af95004a
Mohamed Basrai <mohamedalibasrai@gmail.com>
2021-12-18 01:54:00 -0800

    end resolver

--- src/context/Web3Provider.tsx
+++ src/context/Web3Provider.tsx
@@ -17,11 +17,13 @@ interface Web3ContextInterface {
   signalingSocket?: Contract;
   connectWallet: () => void;
   isConnected: boolean;
+  getEnsName: (name: string) => Promise<string>;
 }
 
 const Web3Context = createContext<Web3ContextInterface>({
   connectWallet: () => {},
   isConnected: false,
+  getEnsName: () => new Promise<string>((resolve, reject) => {}),
 });
 
 // eslint-disable-next-line react/display-name
@@ -32,30 +34,58 @@ export const Web3Provider = memo(
     const [web3Websocket, setWeb3Websocket] = useState<Web3 | undefined>(
       undefined
     );
+    const [web3Ens, setWeb3Ens] = useState<Web3 | undefined>(undefined);
     const [signaling, setSignaling] = useState<Contract | undefined>(undefined);
     const [signalingSocket, setSignalingSocket] = useState<
       Contract | undefined
     >(undefined);
 
+    const initWeb3 = useCallback(async () => {
+      const web3Var = new Web3((window as any).ethereum);
+      const web3WebsockterVar = new Web3(
+        new Web3.providers.WebsocketProvider(
+          "wss://matic-mainnet-archive-ws.bwarelabs.com"
+        )
+      );
+
+      setWeb3(web3Var);
+      setWeb3Websocket(web3WebsockterVar);
+      setSignaling(new web3Var.eth.Contract(SignalingABI, SignalingAddress));
+      setSignalingSocket(
+        new web3WebsockterVar.eth.Contract(SignalingABI, SignalingAddress)
+      );
+
+      setWeb3Ens(new Web3("https://cloudflare-eth.com"));
+    }, []);
+
+    const getEnsName = useCallback(
+      (name: string) => {
+        return new Promise<string>((resolve, reject) => {
+          if (!web3Ens) {
+            reject();
+            return;
+          }
+          web3Ens.eth.ens.getAddress(name, (error: Error, address: string) => {
+            console.log({ address, error });
+            if (address) {
+              resolve(address);
+            } else {
+              reject();
+            }
+          });
+        });
+      },
+      [web3Ens]
+    );
+
     useEffect(() => {
-      const isConnected = (window as any).ethereum?.isConnected();
+      const isConnected = (window as any).ethereum.isConnected();
+      console.log({ isConnected });
       setConnected(isConnected);
       if (isConnected) {
-        const web3Var = new Web3((window as any).ethereum);
-        const web3WebsockterVar = new Web3(
-          new Web3.providers.WebsocketProvider(
-            "wss://matic-mainnet-archive-ws.bwarelabs.com"
-          )
-        );
-
-        setWeb3(web3Var);
-        setWeb3Websocket(web3WebsockterVar);
-        setSignaling(new web3Var.eth.Contract(SignalingABI, SignalingAddress));
-        setSignalingSocket(
-          new web3WebsockterVar.eth.Contract(SignalingABI, SignalingAddress)
-        );
+        initWeb3();
       }
-    }, []);
+    }, [initWeb3]);
 
     const connectWallet = useCallback(async () => {
       try {
@@ -65,23 +95,11 @@ export const Web3Provider = memo(
         await promise;
 
         setConnected(true);
-        const web3Var = new Web3((window as any).ethereum);
-        const web3WebsockterVar = new Web3(
-          new Web3.providers.WebsocketProvider(
-            "wss://matic-mainnet-archive-ws.bwarelabs.com"
-          )
-        );
-
-        setWeb3(web3Var);
-        setWeb3Websocket(web3WebsockterVar);
-        setSignaling(new web3Var.eth.Contract(SignalingABI, SignalingAddress));
-        setSignalingSocket(
-          new web3WebsockterVar.eth.Contract(SignalingABI, SignalingAddress)
-        );
+        initWeb3();
       } catch (error) {
         console.log(error);
       }
-    }, []);
+    }, [initWeb3]);
 
     return (
       <Web3Context.Provider
@@ -92,6 +110,7 @@ export const Web3Provider = memo(
           web3Websocket,
           signaling,
           signalingSocket,
+          getEnsName,
         }}
       >
         {children}
